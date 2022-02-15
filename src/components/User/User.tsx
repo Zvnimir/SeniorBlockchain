22a2a51ecbea398f4cf11b7f6200e9fddaec5a3e
ds1599 <60734979+ds1599@users.noreply.github.com>
2022-02-15 17:13:38 +0100

    Update for User page

--- src/components/User/User.tsx
+++ src/components/User/User.tsx
@@ -20,144 +20,215 @@ const SMART_CONTRACT_ABI = require('../config');
 const SMART_CONTRACT_ADDRESS = require('../config');
 
 
-export class User extends React.Component<{},any> {
+export class User extends React.Component<{}, any> {
 
-state = {username: ''}
-   
+   state = { username: '' }
+
    componentWillMount() {
       this.loadBlockchainData()
-    }
-  
-    async loadBlockchainData() {
-       //result;
+   }
+
+   async loadBlockchainData() {
+      //result;
       const web3 = new Web3(Web3.givenProvider || "http://localhost:8545")
       const accounts = await web3.eth.getAccounts()
       const contract = new web3.eth.Contract(SMART_CONTRACT_ABI.SMART_CONTRACT_ABI, SMART_CONTRACT_ADDRESS)
-      contract.options.address =  "0xC4fF6Dde93E2BF20Cf26923582F0aDab4816304D"
+      contract.options.address = "0xC4fF6Dde93E2BF20Cf26923582F0aDab4816304D"
       this.setState({ contract })
       const userCount = await contract.methods.users("0xE0B6e5538CE13841B19A022cA671a1177a3B7d83").call({ from: accounts[0] })
-      this.setState({ username: userCount})
+      this.setState({ username: userCount })
       console.log(userCount)
-       }
-      //this.setState({ userCount})
-      //this.setState({ account: "userCount"})
-      //console.log(userCount)
-      //console.log(web3.eth.getAccounts())
-    //}
+   }
+   //this.setState({ userCount})
+   //this.setState({ account: "userCount"})
+   //console.log(userCount)
+   //console.log(web3.eth.getAccounts())
+   //}
 
 
-   
-    constructor(props: {} | Readonly<{}>) {
+
+   constructor(props: {} | Readonly<{}>) {
       super(props);
       //this.state = { account: '' }
-  }
-   
-   
- 
-  render() { 
-  return (
-  <div className="App" >
-     <Typography variant="h3" component="div">
-        User Page
-     </Typography>
-     <div className="centered">
-        <Grid container spacing={20}>
-           <Grid item xs="auto">
-              <Card sx={{ maxWidth: 345 }}>
-              <Avatar  alt="The image" src={"user.png"}  className="userPicture" style={{
-              width: 150,
-              height: 150,
-              }} />
-              <CardContent>
-                 <Typography gutterBottom variant="h5" component="div">
-                  { //this.state.username.userEmail 
-                  }
-                 </Typography>
-                 <Typography variant="body2" color="text.secondary">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                    Nullam aliquet nunc blandit ultricies sodales. 
-                    Nulla nec lacus id diam finibus porttitor.
-                 </Typography>
-              </CardContent>
-              <CardActions className="buttons">
-                 <Button size="small">View Balnce</Button>
-                 <Button size="small">Edit</Button>
-              </CardActions>
-              </Card>
-           </Grid>
-           <Grid item xs={6}>
-              <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-              <ListItem alignItems="flex-start">
-                 <ListItemAvatar>
-                    <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
-                 </ListItemAvatar>
-                 <ListItemText
-                 primary="Paper 1"
-                 secondary={
-                 <React.Fragment>
-                    <Typography
-                    sx={{ display: 'inline' }}
-                    component="span"
-                    variant="body2"
-                    color="text.primary"
-                    >
-                    Biology
-                    </Typography>
-                    {" - Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.…"}
-                 </React.Fragment>
-                 }
-                 />
-              </ListItem>
-              <Divider variant="inset" component="li" />
-              <ListItem alignItems="flex-start">
-                 <ListItemAvatar>
-                    <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
-                 </ListItemAvatar>
-                 <ListItemText
-                 primary="Paper 2"
-                 secondary={
-                 <React.Fragment>
-                    <Typography
-                    sx={{ display: 'inline' }}
-                    component="span"
-                    variant="body2"
-                    color="text.primary"
-                    >
-                    Math
-                    </Typography>
-                    {" - Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.…"}
-                 </React.Fragment>
-                 }
-                 />
-              </ListItem>
-              <Divider variant="inset" component="li" />
-              <ListItem alignItems="flex-start">
-                 <ListItemAvatar>
-                    <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
-                 </ListItemAvatar>
-                 <ListItemText
-                 primary="Paper 2"
-                 secondary={
-                 <React.Fragment>
-                    <Typography
-                    sx={{ display: 'inline' }}
-                    component="span"
-                    variant="body2"
-                    color="text.primary"
-                    >
-                    Java
-                    </Typography>
-                    {" - Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.…"}
-                 </React.Fragment>
-                 }
-                 />
-              </ListItem>
-              </List>
-           </Grid>
-        </Grid>
-     </div>
-  </div>
-  );
-  }
-  }
-  
+   }
+
+
+
+   render() {
+      return (
+         <div className="App" >
+            <Typography variant="h3" component="div">
+               User Page
+            </Typography>
+            <div className="centered">
+               <Grid container spacing={20}>
+
+                  <Grid item xs={6}>
+                     <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper', overflow: 'auto', maxHeight: 350, '& ul': { padding: 0 }, }}>
+                        <ListItem alignItems="flex-start">
+                           <ListItemAvatar>
+                              <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+                           </ListItemAvatar>
+                           <ListItemText
+                              primary="Paper 1"
+                              secondary={
+                                 <React.Fragment>
+                                    <Typography
+                                       sx={{ display: 'inline' }}
+                                       component="span"
+                                       variant="body2"
+                                       color="text.primary"
+                                    >
+                                       Biology
+                                    </Typography>
+                                    {" - Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.…"}
+                                 </React.Fragment>
+                              }
+                           />
+                        </ListItem>
+                        <Divider variant="inset" component="li" />
+                        <ListItem alignItems="flex-start">
+                           <ListItemAvatar>
+                              <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
+                           </ListItemAvatar>
+                           <ListItemText
+                              primary="Paper 2"
+                              secondary={
+                                 <React.Fragment>
+                                    <Typography
+                                       sx={{ display: 'inline' }}
+                                       component="span"
+                                       variant="body2"
+                                       color="text.primary"
+                                    >
+                                       Math
+                                    </Typography>
+                                    {" - Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.…"}
+                                 </React.Fragment>
+                              }
+                           />
+                        </ListItem>
+                        <Divider variant="inset" component="li" />
+                        <ListItem alignItems="flex-start">
+                           <ListItemAvatar>
+                              <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
+                           </ListItemAvatar>
+                           <ListItemText
+                              primary="Paper 2"
+                              secondary={
+                                 <React.Fragment>
+                                    <Typography
+                                       sx={{ display: 'inline' }}
+                                       component="span"
+                                       variant="body2"
+                                       color="text.primary"
+                                    >
+                                       Java
+                                    </Typography>
+                                    {" - Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.…"}
+                                 </React.Fragment>
+                              }
+                           />
+                        </ListItem>
+
+                        <Divider variant="inset" component="li" />
+                        <ListItem alignItems="flex-start">
+                           <ListItemAvatar>
+                              <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
+                           </ListItemAvatar>
+                           <ListItemText
+                              primary="Paper 2"
+                              secondary={
+                                 <React.Fragment>
+                                    <Typography
+                                       sx={{ display: 'inline' }}
+                                       component="span"
+                                       variant="body2"
+                                       color="text.primary"
+                                    >
+                                       Math
+                                    </Typography>
+                                    {" - Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.…"}
+                                 </React.Fragment>
+                              }
+                           />
+                        </ListItem>
+                        <Divider variant="inset" component="li" />
+                        <ListItem alignItems="flex-start">
+                           <ListItemAvatar>
+                              <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
+                           </ListItemAvatar>
+                           <ListItemText
+                              primary="Paper 2"
+                              secondary={
+                                 <React.Fragment>
+                                    <Typography
+                                       sx={{ display: 'inline' }}
+                                       component="span"
+                                       variant="body2"
+                                       color="text.primary"
+                                    >
+                                       Math
+                                    </Typography>
+                                    {" - Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.…"}
+                                 </React.Fragment>
+                              }
+                           />
+                        </ListItem>
+                        <Divider variant="inset" component="li" />
+                        <ListItem alignItems="flex-start">
+                           <ListItemAvatar>
+                              <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
+                           </ListItemAvatar>
+                           <ListItemText
+                              primary="Paper 2"
+                              secondary={
+                                 <React.Fragment>
+                                    <Typography
+                                       sx={{ display: 'inline' }}
+                                       component="span"
+                                       variant="body2"
+                                       color="text.primary"
+                                    >
+                                       Math
+                                    </Typography>
+                                    {" - Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.…"}
+                                 </React.Fragment>
+                              }
+                           />
+                        </ListItem>
+                     </List>
+                  </Grid>
+
+                  <Grid item xs="auto">
+                     <Card sx={{ maxWidth: 345 }}>
+                        <Avatar alt="The image" src={"user.png"} className="userPicture" style={{
+                           width: 150,
+                           height: 150,
+                           margin: 'auto',
+                           marginTop: 25
+                        }} />
+                        <CardContent>
+                           <Typography gutterBottom variant="h5" component="div">
+                              { //this.state.username.userEmail 
+                              }
+                              
+                           </Typography>
+                           <Typography variant="body2" color="text.secondary">
+                              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                              Nullam aliquet nunc blandit ultricies sodales.
+                              Nulla nec lacus id diam finibus porttitor.
+                           </Typography>
+                        </CardContent>
+                        <CardActions className="buttons" style={{ display: "flex",  justifyContent: "flex-end" }}>
+                           <Button size="small">View Balnce</Button>
+                           <Button size="small">Edit</Button>
+                        </CardActions>
+                     </Card>
+                  </Grid>
+               </Grid>
+            </div>
+         </div>
+      );
+   }
+}
