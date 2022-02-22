2529ce2702d79bec4a135ae4f723f316a9158761
VegaHaklicka <vh6024@g.rit.edu>
2022-02-22 10:59:17 +0100

    Update Admin.tsx

--- src/components/Admin/Admin.tsx
+++ src/components/Admin/Admin.tsx
@@ -10,6 +10,19 @@ function Admin(){
                     <div className="base-container">
                         <div className="header">Admin</div>
                         <div className="content">
+                            <div className="row">
+                            <article className="article">
+                            <h2 className="article__title">User name... </h2>
+                            <button type="button" className="btn">Approve</button>
+                            </article>
+                            </div>
+
+                            <div className="row">
+                            <article className="article">
+                            <h2 className="article__title">User name... </h2>
+                            <button type="button" className="btn">Approve</button>
+                            </article>
+                            </div>
                             
                         </div>
                     </div>
