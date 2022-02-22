dc7639c4cf0d0d365e78b2b7ed9074141bd1aa0a
VegaHaklicka <vh6024@g.rit.edu>
2022-02-22 14:56:08 +0100

    Update Admin.tsx

--- src/components/Admin/Admin.tsx
+++ src/components/Admin/Admin.tsx
@@ -1,8 +1,77 @@
-import { Card, CardContent, Container, TableCell, TableRow } from "@mui/material";
+import { Box, Button, Card, CardContent, Container, Modal, TableCell, TableRow } from "@mui/material";
+// import Popup from './Popup';
+import React, { useState } from "react";
+import { styled } from '@mui/material/styles';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import DialogActions from '@mui/material/DialogActions';
+import IconButton from '@mui/material/IconButton';
+import SinglePage from '../../domain/singe-page-pdf';
 
-import React from "react";
+
+
+
+const BootstrapDialog = styled(Dialog)(({ theme }) => ({
+    '& .MuiDialogContent-root': {
+      padding: theme.spacing(2),
+    },
+    '& .MuiDialogActions-root': {
+      padding: theme.spacing(1),
+    },
+  }));
+  
+  export interface DialogTitleProps {
+    id: string;
+    children?: React.ReactNode;
+    onClose: () => void;
+  }
+  
+  const BootstrapDialogTitle = (props: DialogTitleProps) => {
+    const { children, onClose, ...other } = props;
+  
+    return (
+      <DialogTitle sx={{ m: 0, p: 2 }} {...other}>
+        {children}
+        {onClose ? (
+          <IconButton
+            aria-label="close"
+            onClick={onClose}
+            sx={{
+              position: 'absolute',
+              right: 8,
+              top: 8,
+              color: (theme) => theme.palette.grey[500],
+            }}
+          >
+          
+          </IconButton>
+        ) : null}
+      </DialogTitle>
+    );
+  };
+
 
 function Admin(){
+
+    const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+
+//   function onDocumentLoadSuccess({ numPages }) {
+//     setNumPages(numPages);
+//   }
+
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = () => {
+      setOpen(true);
+    };
+    const handleClose = () => {
+      setOpen(false);
+    };
+  
+
+
   
       return (
         
@@ -17,13 +86,20 @@ function Admin(){
                                     <p className="">User name... </p>
                                     </TableCell>
                                     <TableCell>
-                                    <button type="button" className="btn">Approve</button>
+                                        <button type="button" className="btn"  onClick={handleClickOpen} >View Document</button>
+                                    </TableCell>
+                                    <TableCell>
+                                    <button type="button" className="btn" >Approve</button>
+                                   
                                     </TableCell>
                                 </TableRow>
 
                                 <TableRow>
                                     <TableCell>
                                     <p className="header">User name... </p>
+                                    </TableCell>
+                                    <TableCell>
+                                        <button type="button" className="btn"  onClick={handleClickOpen} >View Document</button>
                                     </TableCell>
                                     <TableCell>
                                     <button type="button" className="btn">Approve</button>
@@ -33,7 +109,42 @@ function Admin(){
                             </Container>
                             </CardContent>
                 </Card>
+
+
+                      
+                  
+                  
+
+            
+                
+       
+                <BootstrapDialog
+        onClose={handleClose}
+        aria-labelledby="customized-dialog-title"
+        open={open}
+      >
+        <BootstrapDialogTitle id="customized-dialog-title" onClose={handleClose}>
+          Document
+        </BootstrapDialogTitle>
+        <DialogContent dividers>
+        <div>
+        <SinglePage pdf="sample.pdf"></SinglePage>
         </div>
+       
+                  
+
+        </DialogContent>
+        <DialogActions>
+          <Button autoFocus onClick={handleClose}>
+            Close
+          </Button>
+        </DialogActions>
+      </BootstrapDialog>
+
+
+     
+  </div>
+     
 
       );
     
