2d0e67792f5678eda8ff2bb61bcebf33dc4f46b8
Dora <db3901@rit.edu>
2022-02-25 20:02:35 +0100

    Displaying all papers and getting array from bc

--- src/components/Newsfeed/Newsfeed.tsx
+++ src/components/Newsfeed/Newsfeed.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from 'react'
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import Divider from '@mui/material/Divider';
@@ -6,26 +6,97 @@ import ListItemText from '@mui/material/ListItemText';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
 import Avatar from '@mui/material/Avatar';
 import Typography from '@mui/material/Typography';
-import { Box, Button, CardActions, CardContent, Grid } from "@mui/material";
+import { Card, Button, CardActions, CardContent, Grid, Box } from "@mui/material";
 import { Container } from '@mui/material';
 
-import './Newsfeed.css';
+import Web3 from 'web3';
+import { Paper } from '../../model/Paper'
+import { loadBlockchainData } from '../../domain/blockchain-connector';
+
+
+
 
 
 const SMART_CONTRACT_ABI = require('../config');
 const SMART_CONTRACT_ADDRESS = require('../config');
 
+const bull = (
+    <Box
+        component="span"
+        sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+    >
+    </Box>
+);
 
-export class Newsfeed extends React.Component {
-    render() {
-        return (
-          <div>
-            <Container maxWidth="sm">
 
-                Hello from  the other side
-
-            </Container>
-          </div>
-        );
-    }
+type PapersProps = {
+    papers: Paper[]
 }
+
+
+function Newsfeed({ papers }: PapersProps) {
+
+
+    const[paperState, setPaperState] = useState(papers)
+    const [loading, setLoading] = useState(true);
+
+    useEffect( () => {
+        //gets data from blockchain
+        console.log("Someting")
+        loadBlockchainData<Paper[]>("papers").then(result => {
+           if(result) {
+            setPaperState(result)
+            console.log(result);
+            result.forEach((person) => { console.log(person); });
+
+            console.log(paperState[0].title);
+        } 
+        //once we get the data we set loading to false
+        }).finally(() => {
+           setLoading(false);
+         });
+     }, []);
+
+
+     //makes sure that undefined states dont throw errors
+   if (loading) {
+    return <p>Data is loading...</p>;
+    }
+
+    return (
+
+
+       
+
+        <div>
+            <Container maxWidth="md">
+
+                <Card sx={{ minWidth: 275, marginTop: 2 }}>
+                    <CardContent>
+                        <Typography sx={{ fontSize: 14, backgroundColor: 'success.main', borderRadius: 5, maxWidth: 150, padding: 1, textAlign: 'center', color: 'white' }} gutterBottom>
+                            Computer Science
+                        </Typography>
+                        <Typography variant="h5" component="div">
+                        { 
+
+                        paperState[0].title
+                        }
+                        
+                            Achieving Efficient Structured Concurrency through Lightweight Fibers in Java Virtual Machine
+
+                        </Typography>
+                        <Typography variant="body2">
+                            Contemporary concurrent server applications, commonly built of smaller and independent services, are using concurrent threads to serve many incoming requests and often have to perform under excessive load. Those applications are relatively easy to develop in general-purpose, imperative programming languages such as Java, and have great tooling support. However, such applications are not easily scalable, mostly due to relying on oversized OS kernel threads, which can be created only in a limited number on finite hardware resources.
+                        </Typography>
+                    </CardContent>
+                    <CardActions>
+                        <Button size="small">Learn More</Button>
+                    </CardActions>
+                </Card>
+            </Container>
+        </div>
+    );
+}
+
+export default Newsfeed;
+
--- src/domain/router.js
+++ src/domain/router.js
@@ -12,27 +12,27 @@ import { AppBar, Avatar, Box, Button, IconButton, Toolbar, Typography } from '@m
 import FileUploadOutlinedIcon from '@mui/icons-material/FileUploadOutlined';
 import { useNavigate } from 'react-router-dom';
 import UploadPaper from '../components/Paper/UploadPaper';
-import {Newsfeed} from '../components/Newsfeed/Newsfeed';
+import Newsfeed from '../components/Newsfeed/Newsfeed';
 
 function Router() {
 
 
   return (
-    <div>
+    <>
 
       <Navbar />
-      
+
       <Routes>
-        <Route exact path="/" element={ <App/> } />
-        <Route exact path="/paper" element={ <PaperDisplay title={"Test Paper"} domain={"Domain"} description={"Description"}/> } />
-        <Route exact path='/uploadPaper' element={ <UploadPaper></UploadPaper> } />
-        <Route exact path="/register" element={ <Register title={"Test Register"} domain={"Domain"} description={"Description"}/> } />
-        <Route exact path="/login" element={ <Login title={"Test Login"} domain={"Domain"} description={"Description"}/> } />
-        <Route exact path="/user" element={ <UserDisplay title={"Test user"} domain={"Domain"} description={"Description"}/> } />
-        <Route exact path='/admin' element={ <Admin></Admin> } />
-        <Route exact path="/newsfeed" element={ <Newsfeed title={"Test Newsfeed"} domain={"Domain"} description={"Description"}/> } />
+        <Route exact path="/" element={<App />} />
+        <Route exact path="/paper" element={<PaperDisplay title={"Test Paper"} domain={"Domain"} description={"Description"} />} />
+        <Route exact path='/uploadPaper' element={<UploadPaper></UploadPaper>} />
+        <Route exact path="/register" element={<Register title={"Test Register"} domain={"Domain"} description={"Description"} />} />
+        <Route exact path="/login" element={<Login title={"Test Login"} domain={"Domain"} description={"Description"} />} />
+        <Route exact path="/user" element={<UserDisplay title={"Test user"} domain={"Domain"} description={"Description"} />} />
+        <Route exact path='/admin' element={<Admin></Admin>} />
+        <Route exact path="/newsfeed" element={<Newsfeed title={"Test Newsfeed"} domain={"Domain"} description={"Description"} />} />
       </Routes>
-    </div>
+    </>
   );
 };
 
