85ecd8f984db07744c51a8a539587dc79bab5635
Dora <db3901@rit.edu>
2022-03-06 19:20:04 +0100

    Adding the token connector

--- src/components/User/User.tsx
+++ src/components/User/User.tsx
@@ -7,7 +7,6 @@ import Grid from '@mui/material/Grid';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import Divider from '@mui/material/Divider';
@@ -15,47 +14,64 @@ import ListItemText from '@mui/material/ListItemText';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
 import Avatar from '@mui/material/Avatar';
 import Alert from '@mui/material/Alert';
-import Web3 from 'web3'
 import { User } from '../../model/User';
 import { loadBlockchainData } from '../../domain/blockchain-connector';
+import { loadBlockchainData_token } from '../../domain/blockchain-connector_token';
 import { Paper } from '../../model/Paper'
-const SMART_CONTRACT_ABI = require('../config');
-const SMART_CONTRACT_ADDRESS = require('../config');
+
 
 type UserProps = {
    user: User
    papers: Paper[]
 }
 
-function UserDisplay({user, papers }: UserProps) {
+function UserDisplay({ user, papers }: UserProps) {
 
    const [userState, setUserState] = useState(user)
    const [loading, setLoading] = useState(true);
    const [paperState, setPaperState] = useState(papers)
 
-   useEffect( () => {
+   useEffect(() => {
       //gets data from blockchain
       loadBlockchainData<User>("user").then(result => {
-         if(result) {
+         if (result) {
             setUserState(result)
-         } 
-      //once we get the data we set loading to false
+         }
+         //once we get the data we set loading to false
       }).finally(() => {
          setLoading(false);
-       });
+      });
 
-       loadBlockchainData<Paper[]>("userPapers").then(result => {
-         if(result) {
-          setPaperState(result)
-         // console.log(result);
-          result.forEach((person) => { console.log(person); });
+      loadBlockchainData<Paper[]>("userPapers").then(result => {
+         if (result) {
+            setPaperState(result)
+            // console.log(result);
+            result.forEach((person) => { console.log(person); });
 
-          //console.log(paperState[0].title);
-      } 
-      //once we get the data we set loading to false
+            //console.log(paperState[0].title);
+         }
+         //once we get the data we set loading to false
       }).finally(() => {
          setLoading(false);
-       });
+      });
+
+      loadBlockchainData_token<number>("balance").then(result => {
+         if (result) {
+            // userState.balance = result;
+            console.log(result);
+            //result.forEach((person) => { console.log(person); });
+            // userState.balance = result;
+            //console.log(paperState[0].title);
+         }
+         else {
+            console.log("Sipak")
+         }
+         //once we get the data we set loading to false
+      }).finally(() => {
+         setLoading(false);
+      });
+
+
    }, []);
 
 
@@ -64,79 +80,79 @@ function UserDisplay({user, papers }: UserProps) {
       return <p>Data is loading...</p>;
    }
 
-  return (
-   <div className="App" >
-   <Typography variant="h3" component="div">
-      User Page
-   </Typography>
-   <div className="centered">
-      <Grid container spacing={20}>    
-         <Grid item xs={6}>
-        
-           
-            <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper', overflow: 'auto', maxHeight: 350, '& ul': { padding: 0 }, }}>
-            { 
-           paperState.map(paper => (
-              <ListItem alignItems="flex-start">
-              <ListItemAvatar>
-                 <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
-              </ListItemAvatar>
-              <ListItemText
-                 primary={ paper.title}
-                 secondary={
-                    <React.Fragment>
-                       <Typography
-                          sx={{ display: 'inline' }}
-                          component="span"
-                          variant="body2"
-                          color="text.primary"
-                       >
-                         { paper.category}
-                       </Typography>
-                       {"- Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam aliquet nunc blandit ultricies sodales Nulla nec lacus id diam finibus porttitor."}
-                    </React.Fragment>
-                 }
-              />
-           </ListItem>
-    
-       
-           ))}
-             
-           </List>
-          
-          </Grid>
+   return (
+      <div className="App" >
+         <Typography variant="h3" component="div">
+            User Page
+         </Typography>
+         <div className="centered">
+            <Grid container spacing={20}>
+               <Grid item xs={6}>
 
-         <Grid item xs="auto">
-            <Card sx={{ maxWidth: 345 }}>
-            <Alert severity="error">The user is not confirmed</Alert>
 
-               <Avatar alt="The image" src={"user.png"} className="userPicture" style={{
-                  width: 150,
-                  height: 150,
-                  margin: 'auto',
-                  marginTop: 25
-               }} />
-               <CardContent>
-                  <Typography gutterBottom variant="h5" component="div">
-                     { userState.firstName }  { userState.lastName }
-                  </Typography>
+                  <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper', overflow: 'auto', maxHeight: 350, '& ul': { padding: 0 }, }}>
+                     {
+                        paperState.map(paper => (
+                           <ListItem alignItems="flex-start">
+                              <ListItemAvatar>
+                                 <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+                              </ListItemAvatar>
+                              <ListItemText
+                                 primary={paper.title}
+                                 secondary={
+                                    <React.Fragment>
+                                       <Typography
+                                          sx={{ display: 'inline' }}
+                                          component="span"
+                                          variant="body2"
+                                          color="text.primary"
+                                       >
+                                          {paper.category}
+                                       </Typography>
+                                       {"- Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam aliquet nunc blandit ultricies sodales Nulla nec lacus id diam finibus porttitor."}
+                                    </React.Fragment>
+                                 }
+                              />
+                           </ListItem>
 
-                  <Typography variant="body2" color="text.secondary">
-                     Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                     Nullam aliquet nunc blandit ultricies sodales.
-                     Nulla nec lacus id diam finibus porttitor.
-                  </Typography>
-               </CardContent>
-               <CardActions className="buttons" style={{ display: "flex",  justifyContent: "flex-end" }}>
-                  <Button size="small">View Balnce</Button>
-                  <Button size="small">Edit</Button>
-               </CardActions>
-            </Card>
-         </Grid>
-      </Grid>
-   </div>
-</div>
+
+                        ))}
+
+                  </List>
+
+               </Grid>
+
+               <Grid item xs="auto">
+                  <Card sx={{ maxWidth: 345 }}>
+                     <Alert severity="error">The user is not confirmed</Alert>
+
+                     <Avatar alt="The image" src={"user.png"} className="userPicture" style={{
+                        width: 150,
+                        height: 150,
+                        margin: 'auto',
+                        marginTop: 25
+                     }} />
+                     <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                           {userState.firstName}  {userState.lastName}
+                        </Typography>
+
+                        <Typography variant="body2" color="text.secondary">
+                           Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                           Nullam aliquet nunc blandit ultricies sodales.
+                           Nulla nec lacus id diam finibus porttitor.
+                        </Typography>
+                     </CardContent>
+                     <CardActions className="buttons" style={{ display: "flex", justifyContent: "flex-end" }}>
+                        <Button size="small">View Balnce</Button>
+                        <Button size="small">Edit</Button>
+                     </CardActions>
+                  </Card>
+               </Grid>
+            </Grid>
+         </div>
+      </div>
    );
-  }
-  
+}
+
 export default UserDisplay;
---
+++ src/domain/blockchain-connector_token.tsx
@@ -0,0 +1,26 @@
+import Web3 from 'web3'
+import { Paper } from '../model/Paper';
+import { User } from '../model/User';
+
+const SMART_CONTRACT_ABI = require('../components/config_token');
+const SMART_CONTRACT_ADDRESS = require('../components/config_token');
+
+export async function loadBlockchainData_token<Type>(dataType: String, data?: Array<any>): Promise<Type | null>{
+    const web3 = new Web3(Web3.givenProvider || "http://localhost:8545")
+    const accounts = await web3.eth.getAccounts()
+    const contract = new web3.eth.Contract(SMART_CONTRACT_ABI.SMART_CONTRACT_ABI, SMART_CONTRACT_ADDRESS)
+    contract.options.address =  '0x391FeFB1F69743999cb6102bDEB15bF2DA8099FC'
+    
+    switch(dataType) {
+        //user
+        case "balance": {
+            const result: Type = await contract.methods.balanceOf("0xE0B6e5538CE13841B19A022cA671a1177a3B7d83").call({ from: accounts[0] })
+            console.log(result)
+            return result
+        }
+        
+        default: {
+            return null
+        }
+    }
+ }
